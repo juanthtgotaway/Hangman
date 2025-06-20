8536efc8b60b2d78482b256003a1093a200fe32f
Juan Acevedo <jmamartinez18@gmail.com>
2025-06-19 19:36:50 -0600

    generating blanks for selected word

--- ReplacingBlanks/main.py
+++ ReplacingBlanks/main.py
@@ -4,6 +4,13 @@ word_list  = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+
+print(placeholder)
+
 guess = input("Guess a Letter: ").lower()
 print(guess)
 
