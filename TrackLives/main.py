71975621049bf2b957f4113e6f5da3d43139258c
Juan Acevedo <jmamartinez18@gmail.com>
2025-06-23 19:38:13 -0600

    added base and solutiuon to track lives

--- TrackLives/main.py
+++ TrackLives/main.py
@@ -1,6 +1,66 @@
 import random
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+word_list = ["aardvark", "baboon", "camel"]
 
-word_list  = ["aardvark", "baboon", "camel"]
+# TODO-1: - Create a variable called 'lives' to keep track of the number of lives left.
+#  Set 'lives' to equal 6.
+lives = 6
+
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
@@ -8,17 +68,15 @@ placeholder = ""
 word_length = len(chosen_word)
 for position in range(word_length):
     placeholder += "_"
-
 print(placeholder)
 
 game_over = False
-
 correct_letters = []
-while not game_over:
-    guess = input("Guess a Letter: ").lower()
-    # print(guess)
 
-    display =""
+while not game_over:
+    guess = input("Guess a letter: ").lower()
+
+    display = ""
 
     for letter in chosen_word:
         if letter == guess:
@@ -31,6 +89,19 @@ while not game_over:
 
     print(display)
 
-if "_" not in display:
-    game_over = True
-    print("You Win.")
+    # TODO-2: - If guess is not a letter in the chosen_word, Then reduce 'lives' by 1.
+    #  If lives goes down to 0 then the game should stop and it should print "You lose."
+
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            game_over = True
+            print("You loose.")
+
+    if "_" not in display:
+        game_over = True
+        print("You win.")
+
+    # TODO-3: - print the ASCII art from 'stages'
+    #  that corresponds to the current number of 'lives' the user has remaining.
+    print(stages[lives])
